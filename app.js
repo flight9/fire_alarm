d180c726da8845ec6972ef30266f229e85114cad
flight9 <ealeaf@sohu.com>
2018-05-17 14:47:05 +0800

    use local resource

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
 
 app.use(bodyParser.urlencoded({    
   extended: false
