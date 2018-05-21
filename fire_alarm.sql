6027422c373b09145d43faf4647ff98973c4985c
flight9 <ealeaf@sohu.com>
2018-05-21 15:36:02 +0800

    change sql

--- fire_alarm.sql
+++ fire_alarm.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 2018-05-18 13:44:45
+-- Generation Time: 2018-05-21 09:34:24
 -- 服务器版本： 5.6.17
--- PHP Version: 5.6.33
+-- PHP Version: 5.5.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -35,15 +35,15 @@ CREATE TABLE IF NOT EXISTS `captchas` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `mobile` (`mobile`),
   KEY `expire` (`expire`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='验证码' AUTO_INCREMENT=23 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='验证码' AUTO_INCREMENT=27 ;
 
 -- --------------------------------------------------------
 
 --
--- 表的结构 `users`
+-- 表的结构 `fire_users`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
+CREATE TABLE IF NOT EXISTS `fire_users` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `mobile` varchar(11) NOT NULL,
   `openid` varchar(50) NOT NULL,
@@ -51,7 +51,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `mobile` (`mobile`,`openid`),
   KEY `super` (`super`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='用户表' AUTO_INCREMENT=14 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='用户表' AUTO_INCREMENT=18 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
